8a80e08ddb26d079a3121062d999a98b0eff885f
tallshadow <tallshadow@hotmail.fr>
2024-05-27 18:51:10 +0100

    Add .env

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -4,19 +4,33 @@ import { sequelize } from '../config/db';
 import categoryRoutes from '../routes/categoryRoutes';
 import templateRoutes from '../routes/templateRoutes';
 import fileRoutes from '../routes/fileRoutes';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+
+
+
+const cors = require('cors');
+
+
+const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
+// Increase the limit for JSON data
 app.use(bodyParser.json({ limit: '50mb' }));
+
+// Increase the limit for URL-encoded data
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use('/api', categoryRoutes, templateRoutes, fileRoutes);
 
-const handler = (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
+app.use('/api', categoryRoutes, templateRoutes, fileRoutes); 
 
-export default handler;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    // Ensure that your database and Sequelize are initialized here if not already done elsewhere in your setup
+    sequelize.authenticate()
+        .then(() => console.log('Database connected'))
+        .catch(err => console.error('Unable to connect to the database:', err));
+});
+
