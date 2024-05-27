76c22eec1dd8f5e13f4559409f7c2ba73c65f74d
tallshadow <tallshadow@hotmail.fr>
2024-05-27 19:58:01 +0100

    Add .env

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -13,7 +13,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 8000;
 
 app.use(express.json());
 app.use(cors());
