a26d3ada04b20edb7ee57d63d420b1490653c0bc
tallshadow <tallshadow@hotmail.fr>
2024-05-28 02:32:57 +0100

    Add .env

--- backend/build/api/index.js
+++ backend/build/api/index.js
@@ -1,28 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const db_1 = require("../config/db");
-const categoryRoutes_1 = __importDefault(require("../routes/categoryRoutes"));
-const templateRoutes_1 = __importDefault(require("../routes/templateRoutes"));
-const fileRoutes_1 = __importDefault(require("../routes/fileRoutes"));
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = (0, express_1.default)();
-const PORT = 8000;
-app.use(express_1.default.json());
-app.use(cors());
-// Increase the limit for JSON data
-app.use(bodyParser.json({ limit: '50mb' }));
-// Increase the limit for URL-encoded data
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use('/api', categoryRoutes_1.default, templateRoutes_1.default, fileRoutes_1.default);
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    // Ensure that your database and Sequelize are initialized here if not already done elsewhere in your setup
-    db_1.sequelize.authenticate()
-        .then(() => console.log('Database connected'))
-        .catch(err => console.error('Unable to connect to the database:', err));
-});
+function handler(req, res) {
+    const { name = 'World' } = req.query;
+    return res.json({
+        message: `Hello ${name}!`,
+    });
+}
+exports.default = handler;
