a1d3b61ee0ed9b88cf95cc5c7722c843e335c236
tallshadow <tallshadow@hotmail.fr>
2024-05-28 19:23:57 +0100

    Add .env

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// import express from 'express';
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8000;
+app.use("/", (req, res) => res.send("Express on Vercel"));
+app.listen(3000, () => console.log("Server ready on port 3000."));
