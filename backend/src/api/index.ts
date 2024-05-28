56d883ed46c6b355be5b5112a67f3ac2f4cd6b76
tallshadow <tallshadow@hotmail.fr>
2024-05-28 01:25:07 +0100

    Add .env

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,36 +1,9 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import express from 'express';
-import { sequelize } from '../config/db';
-import categoryRoutes from '../routes/categoryRoutes';
-import templateRoutes from '../routes/templateRoutes';
-import fileRoutes from '../routes/fileRoutes';
-
-
-
-const cors = require('cors');
-
-
-const bodyParser = require('body-parser');
-
+const express = require("express");
 const app = express();
-const PORT = 8000;
 
-app.use(express.json());
-app.use(cors());
-// Increase the limit for JSON data
-app.use(bodyParser.json({ limit: '50mb' }));
+app.get("/", (req:any, res:any) => res.send("Express on Vercel"));
 
-// Increase the limit for URL-encoded data
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use('/api', categoryRoutes, templateRoutes, fileRoutes); 
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    // Ensure that your database and Sequelize are initialized here if not already done elsewhere in your setup
-    sequelize.authenticate()
-        .then(() => console.log('Database connected'))
-        .catch(err => console.error('Unable to connect to the database:', err));
-});
+module.exports = app;
 
