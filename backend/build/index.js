7421baf4c3a3dfa39260d9baf45ba0a1dd89052d
tallshadow <tallshadow@hotmail.fr>
2024-05-29 18:13:44 +0100

    Add .env

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,6 +1,12 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = (0, express_1.default)();
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,15 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const db_1 = require("./src/config/db");
-const categoryRoutes_1 = __importDefault(require("./src/routes/categoryRoutes"));
-const templateRoutes_1 = __importDefault(require("./src/routes/templateRoutes"));
-const fileRoutes_1 = __importDefault(require("./src/routes/fileRoutes"));
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = (0, express_1.default)();
-const PORT = 8000;
-app.use(express_1.default.json());
-app.use(cors());
-// Increase the limit for JSON data
-app.use(bodyParser.json({ limit: '50mb' }));
-// Increase the limit for URL-encoded data
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use('/api', categoryRoutes_1.default, templateRoutes_1.default, fileRoutes_1.default);
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    // Ensure that your database and Sequelize are initialized here if not already done elsewhere in your setup
-    db_1.sequelize.authenticate()
-        .then(() => console.log('Database connected'))
-        .catch(err => console.error('Unable to connect to the database:', err));
-});
+import express from "express";
+import { sequelize } from "./config/db";
+import categoryRoutes from "./routes/categoryRoutes";
+import templateRoutes from "./routes/templateRoutes";
+import fileRoutes from "./routes/fileRoutes";
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
