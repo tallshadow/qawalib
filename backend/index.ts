6d9323a0ba158eacc433ac6fe38714cbc5a0f787
tallshadow <tallshadow@hotmail.fr>
2024-05-29 19:13:34 +0100

    First Commit

--- backend/index.ts
+++ backend/index.ts
@@ -9,9 +9,25 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.get("/", (req:any, res:any) => res.send("Express on Vercel"));
+const PORT = 8000;
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use(express.json());
+app.use(cors());
+// Increase the limit for JSON data
+app.use(bodyParser.json({ limit: '50mb' }));
 
-module.exports = app;
+// Increase the limit for URL-encoded data
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use('/api', categoryRoutes, templateRoutes, fileRoutes); 
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    // Ensure that your database and Sequelize are initialized here if not already done elsewhere in your setup
+    sequelize.authenticate()
+        .then(() => console.log('Database connected'))
+        .catch(err => console.error('Unable to connect to the database:', err));
+});
+
 
