01f8b40910d46ef079e45e158681b0a01530cd93
tallshadow <tallshadow@hotmail.fr>
2024-06-02 13:08:55 +0100

    Qawalib fixpdf

--- client/src/components/template/TemplatePreview.tsx
+++ client/src/components/template/TemplatePreview.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Template } from "../../types";
 import {
-  Dialog,
   DialogTitle,
   DialogContent,
   DialogActions,
   Button,
-  CircularProgress,
-  Box,
-  Typography,
 } from "@mui/material";
 import ReactQuill from "react-quill";
 import jsPDF from "jspdf";
@@ -36,14 +32,24 @@ const TemplatePreview: React.FC<TemplatePreviewProps> = ({
   const [content, setContent] = useState<string>(fileContent);
   const [isEditing, setIsEditing] = useState(false);
   const [isArabic, setIsArabic] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [workerUrl, setWorkerUrl] = useState<string>("");
   const isPdf = isPdfFile(template.templateFile);
 
   useEffect(() => {
+    const loadPdfJsWorker = async () => {
+      const pdfjsWorker = (await import("pdfjs-dist/build/pdf.worker.entry"))
+        .default;
+      setWorkerUrl(pdfjsWorker);
+    };
+
+    if (isPdf) {
+      loadPdfJsWorker();
+    }
+
+    console.log("**content empty****", content);
     const arabicRegex = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]/;
     setIsArabic(arabicRegex.test(content));
-    setLoading(false);
-  }, [content]);
+  }, [content, isPdf]);
 
   const handleEdit = () => setIsEditing(true);
 
@@ -85,76 +91,40 @@ const TemplatePreview: React.FC<TemplatePreviewProps> = ({
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
-    <Dialog open onClose={onClose} maxWidth="md" fullWidth>
+    <>
       <DialogTitle>{template.name}</DialogTitle>
+
       <DialogContent>
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          minHeight="400px"
-          p={2}
-          sx={{ backgroundColor: "#f0f0f0", borderRadius: 2 }}
-        >
-          {loading ? (
-            <CircularProgress />
-          ) : isEditing && content.trim() ? (
-            <ReactQuill
-              value={content}
-              onChange={setContent}
-              style={{
-                direction: isArabic ? "rtl" : "ltr",
-                textAlign: isArabic ? "right" : "left",
-              }}
-            />
-          ) : isPdf ? (
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-              <Viewer
-                fileUrl={urlToFile}
-                plugins={[defaultLayoutPluginInstance]}
-              />
-            </Worker>
-          ) : (
-            <Box
-              sx={{
-                width: "100%",
-                maxHeight: "500px",
-                overflowY: "auto",
-                backgroundColor: "#ffffff",
-                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-                borderRadius: "5px",
-                p: 3,
-              }}
-              dangerouslySetInnerHTML={{ __html: content }}
-            />
-          )}
-        </Box>
+        {isEditing && content.trim() ? (
+          <ReactQuill
+            value={content}
+            onChange={setContent}
+            style={{
+              direction: isArabic ? "rtl" : "ltr",
+              textAlign: isArabic ? "right" : "left",
+            }}
+          />
+        ) : (
+          <>
+            {isPdf && workerUrl ? (
+              <Worker workerUrl={workerUrl}>
+                <Viewer
+                  fileUrl={urlToFile}
+                  plugins={[defaultLayoutPluginInstance]}
+                />
+              </Worker>
+            ) : (
+              <div dangerouslySetInnerHTML={{ __html: content }} />
+            )}
+          </>
+        )}
       </DialogContent>
       <DialogActions>
-        {isEditing ? (
-          <>
-            <Button onClick={handleSave} color="primary" variant="contained">
-              Save
-            </Button>
-            <Button
-              onClick={handleDownload}
-              color="primary"
-              variant="contained"
-            >
-              Download
-            </Button>
-          </>
-        ) : (
-          <Button onClick={handleEdit} color="primary" variant="contained">
-            Edit
-          </Button>
-        )}
         <Button onClick={onClose} color="secondary" variant="contained">
           Close
         </Button>
       </DialogActions>
-    </Dialog>
+    </>
   );
 };
 
---
+++ client/src/types/pdfjs-dist.d.ts
@@ -0,0 +1,5 @@
+declare module 'pdfjs-dist/build/pdf.worker.entry' {
+    const workerSrc: string;
+    export default workerSrc;
+  }
+  
