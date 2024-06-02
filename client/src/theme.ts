a8daf82938dbe0dab3b944353a4cc5c8431483bd
tallshadow <tallshadow@hotmail.fr>
2024-06-02 19:42:02 +0100

    Qawalib final v1.0

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -86,6 +86,18 @@ const theme = createTheme({
         },
       },
     },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#f5f5f7cc',
+          color: '#1d1d1f',
+          transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+          borderRadius: 4,
+          boxShadow: '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)',
+          overflow: 'hidden',
+        },
+      },
+    },
   },
 });
 
